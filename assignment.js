4072fd9d2682f1f2fd2a4694bf3b49da1a0ad96c
unknown <aszalea_calderon@yahoo.com>
2021-04-17 10:25:12 -0700

    finished assignment

---
+++ assignment.js
@@ -0,0 +1,11 @@
+const userInput = 15;
+let result;
+
+result = 18 + userInput;
+result = 30 - userInput * 30;
+result = 8 * userInput * 30;
+result = 9 + userInput + 2;
+
+alert(result);
+alert(userInput);
+console.log(result);
